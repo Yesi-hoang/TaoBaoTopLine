f9e96d4ab907ceb88ca49c323c068d5c46d65f3f
Yesi <317447721@qq.com>
2016-03-31 10:38:32 +0800

    Initial Commit

---
+++ TaoBaoTopLine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaoBaoTopLine
+//
+//  Created by Yesi on 16/3/31.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
